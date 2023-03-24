d031e66155c84a92eddbdb901373ec8d6239fc79
Brandon Ching <brandonching@MacBook-Air.local>
2023-03-24 10:05:23 -0700

    Added file of a51

---
+++ src/a51.java
@@ -0,0 +1,10 @@
+public class a51 {
+    int xor (int a, int b) {
+        if (a == b) return 0;
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }    
+}
