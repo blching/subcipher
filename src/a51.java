57b5fadb679d93179aaefac48e2fd4ac09c359b2
Brandon Ching <brandonching@MacBook-Air.local>
2023-03-24 10:23:17 -0700

    Input of x y and z

--- src/a51.java
+++ src/a51.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class a51 {
     int xor (int a, int b) {
         if (a == b) return 0;
@@ -5,6 +7,24 @@ public class a51 {
     }
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] x = new int[19];
+        int[] y = new int[22];
+        int[] z = new int[23];
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter x: ");
+        String xstring = scan.nextLine();
+
+        System.out.println("Please enter y: ");
+        String ystring = scan.nextLine();
+
+        System.out.println("Please enter z: ");
+        String zstring = scan.nextLine(); 
+
+        System.out.println(xstring);
+        System.out.println(ystring);
+        System.out.println(zstring);
+
     }    
 }
